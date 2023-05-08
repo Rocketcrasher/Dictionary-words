760ba17b4909e277a695fc3813805dc46e671eca
Vlad Korzun <vlad.korzun.minsk@gmail.com>
2023-05-09 02:24:34 +0300

    Add all functional, min version I wanted

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import { useState } from 'react';
 import './App.css';
 import list from "./data/words.json";
-
-function App() {
-  const [word, setWord] = useState('text');
-
-  let failedWords = [];
+let failedWords = [];
   let count = 0;
+function App() {
+  const [word, setWord] = useState(list[0]);
+  const [field, setField] = useState('Click to say and show answer');
 
   // const transformText = () => {
   //   setWord("some text")
@@ -24,30 +23,38 @@ function App() {
   // }
 
   const changeWord = (ans) => {
-    if (ans) {
-      if (!failedWords.find(elem=>elem == word))
+    setField('Click to say and show answer');
+
+    if (ans && !failedWords.find(elem=>elem == word)) {
         failedWords.push(word);
     }
-    if (count % 5 != 5 * Math.random())
-      setWord(list[Math.round(list.length * Math.random())]);
-    else setWord(failedWords[Math.round(failedWords.length * Math.random())])
-
-    count++;
+    else if (!ans && failedWords.find(elem=>elem == word)) {
+      failedWords.shift();
+    }
+    const k = 4;
+    if (count % k == Math.floor(k * Math.random()) && failedWords.length != 0){
+      setWord(failedWords[0]);
+    }
+    else {
+      setWord(list[Math.floor(list.length * Math.random())]);
+      
+    }
+    count++; 
   }
 
   const sayWord = () => {
     window.speechSynthesis.cancel();
-
+    setField(word);
     window.speechSynthesis.speak(new SpeechSynthesisUtterance(word));
   }
   
   return (
     <div className="App">
       <header className="App-header">
-        <div className='word' onClick={sayWord}>{word}</div>
+        <div className='word' onClick={sayWord}>{field}</div>
         <div>
-          <button className='ansbut' onClick={() => changeWord(1)}>Mistake</button>
-          <button className='ansbut' onClick={() => changeWord(0)}>Right</button>
+          <button className='ansbut' onClick={() => changeWord(1)}>Fail</button>
+          <button className='ansbut' onClick={() => changeWord(0)}>Correct</button>
         </div>
       </header>
     </div>
