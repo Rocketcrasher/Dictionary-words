4480b8aa40e8e7d52b3bf3ecd6c1122783bf2cd3
Vlad Korzun <vlad.korzun.minsk@gmail.com>
2023-05-09 01:19:50 +0300

    Add structure words

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import list from "./data/words.json";
 
 function App() {
+  const [word, setWord] = useState('text');
+
+  let failedWords = [];
+  let count = 0;
+
+  // const transformText = () => {
+  //   setWord("some text")
+  //   //list = "дивиденд, дизайн, дилемма, дилижанс, дирижер, диссонанс, дифирамб, дубликат, иждивенец, инициатива, инцидент, квитанция, криминальный, критерий, кульминация"
+  //   //answer = "\"дивиденд\", \"дизайн\", ..."
+  //   let answer = '';
+  //   let counter = 0;
+  //   for (let i = 0; i < list.length; i++) {
+  //     if (list[i] == ',') {
+  //       answer = answer + '"' + list.slice(counter, i) + '", ';
+  //       counter = i + 2;
+  //     }
+  //   }
+  //   setWord(answer);
+  // }
+
+  const changeWord = (ans) => {
+    if (ans) {
+      if (!failedWords.find(elem=>elem == word))
+        failedWords.push(word);
+    }
+    if (count % 5 != 5 * Math.random())
+      setWord(list[Math.round(list.length * Math.random())]);
+    else setWord(failedWords[Math.round(failedWords.length * Math.random())])
+
+    count++;
+  }
+
+  const sayWord = () => {
+    window.speechSynthesis.cancel();
+
+    window.speechSynthesis.speak(new SpeechSynthesisUtterance(word));
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='word' onClick={sayWord}>{word}</div>
+        <div>
+          <button className='ansbut' onClick={() => changeWord(1)}>Mistake</button>
+          <button className='ansbut' onClick={() => changeWord(0)}>Right</button>
+        </div>
       </header>
     </div>
   );
+
+  
 }
 
 export default App;
